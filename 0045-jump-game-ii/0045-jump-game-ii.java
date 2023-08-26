ee99bdea7394caee61d81377b806b6c6b2498776
Pranav128 <100985846+Pranav128@users.noreply.github.com>
2023-08-26 18:09:41 +0530

    Time: 2 ms (55.42%), Space: 44.1 MB (93.76%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int jump(int[] nums) {
+        return minJumps(nums);
+    }
+    static int minJumps(int[] arr){
+        int n = arr.length ;
+        if(n <= 1)
+        {
+            return 0;
+        }
+        if(arr[0] == 0)
+        {
+            return -1 ;
+        }
+        int jump =  1 , maxReach=arr[0] , steps = arr[0] ;
+        for(int i=1 ;i<n ;i++)
+        {
+            if(i == n-1)
+            {
+                return jump ;
+            }
+            maxReach = Math.max(maxReach , arr[i]+i);
+            steps -- ;
+            if(steps == 0)
+            {
+                jump++ ;
+                if(i >= maxReach) //imp
+                {
+                    return -1 ;
+                }
+                steps = maxReach - i ;
+            }
+         }
+         return -1 ;
+    }
+}
