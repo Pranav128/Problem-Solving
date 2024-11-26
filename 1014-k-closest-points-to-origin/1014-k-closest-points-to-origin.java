3d458f2b028f14f23c436f72391a5b49e1ca26cf
Pranav128 <100985846+Pranav128@users.noreply.github.com>
2024-11-26 15:26:31 +0530

    Time: 24 ms (75.98%), Space: 53.9 MB (36.24%) - LeetHub

---
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
@@ -0,0 +1,33 @@
+class Point{
+    int x,y;
+    int square;
+    Point(int x,int y,int square)
+    {
+        this.x=x;
+        this.y=y;
+        this.square=square;
+    }
+    public int getX(){
+        return this.x;
+    }
+    public int getY(){
+        return this.y;
+    }
+
+}
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        int ans[][]=new int[k][2];
+        PriorityQueue<Point> pq =  new PriorityQueue<Point>((a,b)->a.square-b.square);
+        for(int i=0;i<points.length;i++){
+            int square=points[i][0]*points[i][0] + points[i][1]*points[i][1];
+            pq.offer(new Point(points[i][0],points[i][1],square));
+        }
+        for(int i=0;i<k;i++){
+            Point temp=pq.poll();
+            ans[i][0]=temp.getX();
+            ans[i][1]=temp.getY();
+        }
+        return ans;
+    }
+}
