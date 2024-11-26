ee9e9b307cd805e77ce96bb15f9db9433e4e9a33
Pranav128 <100985846+Pranav128@users.noreply.github.com>
2024-11-26 18:18:39 +0530

    Time: 44 ms (13.01%), Space: 46.8 MB (7.13%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -28,7 +28,7 @@ class Solution {
     }
     
 //Approach 2: Priority Queue 
-    public List<Integer> findClosestElements2(int[] arr, int k, int x) {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
         int n=arr.length;
         List<Integer>ans=new ArrayList<>();
         PriorityQueue<Pair> pq=new PriorityQueue<>((a,b)->{
@@ -54,7 +54,7 @@ class Solution {
     }
 
 //Approach 3: Binary search
-public List<Integer> findClosestElements(int[] arr, int k, int x) {
+public List<Integer> findClosestElements2(int[] arr, int k, int x) {
         int left = 0, right = arr.length - k;
         while (left < right) {
             int midpoint = left + (right - left) / 2; // same as (left + right) / 2
