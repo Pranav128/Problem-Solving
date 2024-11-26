4606d3e52bda0d1f52c872ef267f3fca4bf24090
Pranav128 <100985846+Pranav128@users.noreply.github.com>
2024-11-26 15:31:48 +0530

    Time: 29 ms (64.72%), Space: 55.7 MB (10.05%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
@@ -1,4 +1,4 @@
-class Point{
+class Point implements Comparable<Point>{
     int x,y;
     int square;
     Point(int x,int y,int square)
@@ -13,10 +13,30 @@ class Point{
     public int getY(){
         return this.y;
     }
+    public int compareTo(Point p){
+            return (int)p.square-(int)this.square;
+    }
 
 }
 class Solution {
-    public int[][] kClosest(int[][] points, int k) {
+     public int[][] kClosest(int[][] points, int k) {
+        int ans[][]=new int[k][2];
+        PriorityQueue<Point> pq =  new PriorityQueue<Point>();
+        for(int i=0;i<points.length;i++){
+            int square=points[i][0]*points[i][0] + points[i][1]*points[i][1];
+            pq.offer(new Point(points[i][0],points[i][1],square));
+            while(pq.size()>k){
+                pq.poll();
+            }
+        }
+        for(int i=0;i<k;i++){
+            Point temp=pq.poll();
+            ans[i][0]=temp.getX();
+            ans[i][1]=temp.getY();
+        }
+        return ans;
+    }
+    public int[][] kClosest2(int[][] points, int k) {
         int ans[][]=new int[k][2];
         PriorityQueue<Point> pq =  new PriorityQueue<Point>((a,b)->a.square-b.square);
         for(int i=0;i<points.length;i++){
