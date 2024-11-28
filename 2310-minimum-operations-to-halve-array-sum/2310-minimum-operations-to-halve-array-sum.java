9d78f8db08225712ac8cf322ce7d98aac1ad8e75
Pranav128 <100985846+Pranav128@users.noreply.github.com>
2024-11-28 22:27:31 +0530

    Time: 205 ms (33.48%), Space: 58.7 MB (25.71%) - LeetHub

---
+++ 2310-minimum-operations-to-halve-array-sum/2310-minimum-operations-to-halve-array-sum.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int halveArray(int[] nums) {
+        PriorityQueue<Double> maxHeap=new PriorityQueue<>(Collections.reverseOrder());
+        double arrSum=0;
+        double heapSum=0;
+        int count=0;
+        for(int i:nums){
+            arrSum+=i;
+            maxHeap.offer(Double.valueOf(i));
+        }
+        heapSum=arrSum;
+        while((arrSum-heapSum) < (arrSum/2.0)){
+            double toBeAdd = maxHeap.poll()/2.0;
+            heapSum -=toBeAdd;
+            maxHeap.offer(toBeAdd);
+            count++;
+        }
+        return count;
+    }
+}
