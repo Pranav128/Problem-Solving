ae6eec06bfcbc7e6443440d43e3d91fb7f1316f6
Pranav128 <100985846+Pranav128@users.noreply.github.com>
2024-11-29 12:46:24 +0530

    Time: 10 ms (47.07%), Space: 45.3 MB (19.36%) - LeetHub

--- 1829-maximum-units-on-a-truck/1829-maximum-units-on-a-truck.java
+++ 1829-maximum-units-on-a-truck/1829-maximum-units-on-a-truck.java
@@ -1,20 +1,45 @@
 class Solution {
     public int maximumUnits(int[][] boxTypes, int truckSize) {
-    //    return fractionalKnapsack(boxTypes,truckSize);
-        Arrays.sort(boxTypes,(a,b)->Integer.compare(b[1],a[1]));
-        int profit =0;
-        for(int i=0;i<boxTypes.length;i++){
-            if(boxTypes[i][0] <=  truckSize){
-                profit += (boxTypes[i][0]*boxTypes[i][1]);
-                truckSize -=boxTypes[i][0];
+       return fractionalKnapsack(boxTypes,truckSize);
+    }
+    int fractionalKnapsack(int[][] box,int capacity) {
+        Pair[] arr = new Pair[box.length];
+        for(int i=0;i<box.length;i++){
+            int tempVal=box[i][0];
+            int tempWt=box[i][1];
+            arr[i]=new Pair(tempVal,tempWt);
+        }
+        Arrays.sort(arr);
+
+        if(capacity<arr[0].wt){
+            return 0;
+        }
+        int profit=0;
+        for(Pair i : arr){
+            if(capacity>=i.wt){
+                capacity -=i.wt;
+                profit += (i.wt*i.val);
             }
             else{
-                if(truckSize !=0){
-                    profit  += (truckSize*boxTypes[i][1]);
+                if(capacity !=0){
+                    profit += (capacity*i.val);
                     break;
                 }
             }
         }
         return profit;
+    }
+}
+class Pair implements Comparable<Pair>{
+    int val,wt;
+    public Pair(int wt,int val){
+        this.val=val;
+        this.wt=wt;
+    }
+    public int compareTo(Pair o){
+        if(this.val!=o.val){
+            return (o.val-this.val);
+        }
+        return this.wt-o.wt;
     }
 }
