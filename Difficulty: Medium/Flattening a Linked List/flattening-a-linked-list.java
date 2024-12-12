430d10597b6fa6261a38be15c375dd0e1e7d9e87
Pranav128 <100985846+Pranav128@users.noreply.github.com>
2024-12-12 14:00:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Flattening a Linked List/flattening-a-linked-list.java
+++ Difficulty: Medium/Flattening a Linked List/flattening-a-linked-list.java
@@ -21,7 +21,7 @@ class Node {
 
 class Solution {
     // Function to flatten a linked list
-    Node flatten(Node root) {
+    Node flatten2(Node root) {
         Node curr=root;
         ArrayList<Integer> list= new ArrayList<>();
         while(curr!=null){
@@ -42,6 +42,40 @@ class Solution {
             tail=temp;
         }
         return tempHead.bottom;
+    }
+    
+    public static Node merge(Node list1, Node list2) {
+        Node dummyNode = new Node(-1);
+        Node res = dummyNode;
+        while (list1 != null && list2 != null) {
+            if (list1.data < list2.data) {
+                res.bottom = list1;
+                res = list1;
+                list1 = list1.bottom;
+            } else {
+                res.bottom = list2;
+                res = list2;
+                list2 = list2.bottom;
+            }
+            res.next = null;
+        }
+        if (list1 != null) {
+            res.bottom = list1;
+        } else {
+            res.bottom = list2;
+        }
+        if (dummyNode.bottom != null) {
+            dummyNode.bottom.next = null;
+        }
+        return dummyNode.bottom;
+    }
+    public static Node flatten(Node head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        Node mergedHead = flatten(head.next);
+        head = merge(head, mergedHead);
+        return head;
     }
 }
 
