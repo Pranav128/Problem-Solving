fa3d3b095b9cda3edaf55c5a0e99fe735c32aeb0
Pranav128 <100985846+Pranav128@users.noreply.github.com>
2024-12-25 20:15:28 +0530

    Time: 0 ms (100%), Space: 45.2 MB (23.82%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -14,11 +14,11 @@
  * }
  */
 class Solution {
-public static List<List<Integer>> levelOrder(TreeNode root) {
+    public static List<List<Integer>> levelOrder2(TreeNode root) {
         List<List<Integer>>ans=new ArrayList<>();
-    if(root==null){
-return ans;
-    }
+        if(root==null){
+            return ans;
+        }
         Queue<TreeNode> que=new LinkedList<>();
         List<Integer> tempList=new ArrayList<>();
         que.add(root);
@@ -47,5 +47,50 @@ return ans;
         }
         return ans;
     }
-
+    public static List<List<Integer>> levelOrder3(TreeNode root) {
+        List<List<Integer>>ans=new ArrayList<>();
+        if(root==null){
+            return ans;
+        }
+        Queue<TreeNode> que=new LinkedList<>();
+        que.add(root);
+        while(!que.isEmpty()){
+            int size=que.size();
+            ArrayList<Integer>temp=new ArrayList<>();
+            for(int i=0;i<size;i++){
+                TreeNode polled=que.poll();
+                if(polled.left!=null){
+                    que.add(polled.left);
+                }
+                if(polled.right!=null){
+                    que.add(polled.right);
+                }
+                temp.add(polled.val);
+            }
+            ans.add(temp);
+        }
+        return ans;
+    }
+    public static List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>>ans=new ArrayList<>();
+        if(root==null){
+            return ans;
+        }
+        recursion(root,ans,0);
+        return ans;
+    }
+    public static void recursion(TreeNode root,List<List<Integer>> ans, int height){
+        if(root==null){
+            return ;
+        }
+        if(height==ans.size()){
+            ans.add(new ArrayList<>());
+            ans.get(height).add(root.val);
+        }
+        else{
+            ans.get(height).add(root.val);
+        }
+        recursion(root.left,ans,height+1);
+        recursion(root.right,ans,height+1);
+    }
 }
