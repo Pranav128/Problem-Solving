5441d8f30878d156c4c7d85265c15aa71eb51b7d
Pranav128 <100985846+Pranav128@users.noreply.github.com>
2025-01-09 11:59:40 +0530

    Time: 9 ms (49.51%), Space: 46.9 MB (14.13%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -6,14 +6,14 @@ class Pair{
     }
 }
 class Solution {
-    public int[][] merge2(int[][] arr) {
+    public int[][] merge(int[][] arr) {
         ArrayList<Pair>list=new ArrayList<>();
         Arrays.sort(arr,Comparator.comparingInt(a -> a[0]));
         for(int i=0;i<arr.length;i++){
             if(list.isEmpty() || arr[i][0]>list.get(list.size()-1).end){
                 list.add(new Pair(arr[i][0],arr[i][1]));
             }else{
-                list.get(list.size()-1).end=arr[i][1];
+                list.get(list.size()-1).end=Math.max(arr[i][1],list.get(list.size()-1).end);
             }
         }
         int ans[][]=new int[list.size()][2];
@@ -25,8 +25,8 @@ class Solution {
         }
         return ans;
     }
-
-    public int[][] merge(int[][] intervals) {
+//9ms
+    public int[][] merge2(int[][] intervals) {
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
         int k = 0;
         for (int i = 1; i < intervals.length; i++) {
