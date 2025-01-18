1f0a3722fd21c5df810ad706963ba057af44df71
Pranav128 <100985846+Pranav128@users.noreply.github.com>
2025-01-18 23:15:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Evaluation of Postfix Expression/evaluation-of-postfix-expression.java
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+
+
+//Evaluation of Postfix Expression
+class Solution {
+    public static int evaluate(String[] arr) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (String token : arr) {
+            if (isOperator(token)) {
+                int operand2 = stack.pop();
+                int operand1 = stack.pop();
+                int result = performOperation(token, operand1, operand2);
+                stack.push(result);
+            } else {
+                stack.push(Integer.parseInt(token));
+            }
+        }
+
+        return stack.pop();
+    }
+
+    private static boolean isOperator(String token) {
+        return token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/");
+    }
+
+    private static int performOperation(String operator, int operand1, int operand2) {
+        switch (operator) {
+            case "+":
+                return operand1 + operand2;
+            case "-":
+                return operand1 - operand2;
+            case "*":
+                return operand1 * operand2;
+            case "/":
+                return operand1 / operand2;
+            default:
+                return 0;
+        }
+    }
+}
+
+//{ Driver Code Starts.
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String line = reader.readLine();
+        int t = Integer.parseInt(line);
+        while (t-- > 0) {
+            line = reader.readLine();
+            String[] arr = line.split(" ");
+            Solution solution = new Solution();
+            System.out.println(solution.evaluate(arr));
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
